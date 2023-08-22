6b49ebef197f19f1e9a17efb5af65bc207e6c6cb
Maxwell Gerber <max@stytch.com>
2023-08-21 17:45:32 -0700

    feat: Add feature parity for UpdateBuilder

--- update.go
+++ update.go
@@ -15,62 +15,100 @@ type UpdateBuilder struct {
 	hasChanges bool
 }
 
-func (s UpdateBuilder) Set(column string, value any) UpdateBuilder {
-	return s.
-		withBuilder(s.builder.Set(column, value)).
+// Prefix adds an expression to the beginning of the query
+func (b UpdateBuilder) Prefix(sql string, args ...interface{}) UpdateBuilder {
+	return b.withBuilder(b.builder.Prefix(sql, args...))
+}
+
+// PrefixExpr adds an expression to the very beginning of the query
+func (b UpdateBuilder) PrefixExpr(expr sq.Sqlizer) UpdateBuilder {
+	return b.withBuilder(b.builder.PrefixExpr(expr))
+}
+
+// Set adds SET clauses to the query.
+func (b UpdateBuilder) Set(column string, value any) UpdateBuilder {
+	return b.
+		withBuilder(b.builder.Set(column, value)).
 		withChanges()
 }
 
-func (s UpdateBuilder) SetMap(clauses map[string]interface{}, errors ...error) UpdateBuilder {
+// SetMap is a convenience method which calls Set for each key/value pair in clauses.
+func (b UpdateBuilder) SetMap(clauses map[string]any, errors ...error) UpdateBuilder {
 	for _, err := range errors {
 		if err != nil {
-			return s.withError(err)
+			return b.withError(err)
 		}
 	}
 	if len(clauses) == 0 {
-		return s
+		return b
 	}
-	return s.
-		withBuilder(s.builder.SetMap(clauses)).
+	return b.
+		withBuilder(b.builder.SetMap(clauses)).
 		withChanges()
 }
 
-func (s UpdateBuilder) Where(pred interface{}, rest ...interface{}) UpdateBuilder {
-	return s.withBuilder(s.builder.Where(pred, rest...))
+// Where adds WHERE expressions to the query.
+//
+// See SelectBuilder.Where for more information.
+func (b UpdateBuilder) Where(pred interface{}, rest ...interface{}) UpdateBuilder {
+	return b.withBuilder(b.builder.Where(pred, rest...))
 }
 
-func (s UpdateBuilder) Using(operation func(builder UpdateBuilder) UpdateBuilder) UpdateBuilder {
-	return operation(s)
+// OrderBy adds ORDER BY expressions to the query.
+func (b UpdateBuilder) OrderBy(orderBys ...string) UpdateBuilder {
+	return b.withBuilder(b.builder.OrderBy(orderBys...))
 }
 
-func (s UpdateBuilder) Debug() UpdateBuilder {
-	debug(s.ctx, s.builder)
-	return s
+// Limit sets a LIMIT clause on the query.
+func (b UpdateBuilder) Limit(limit uint64) UpdateBuilder {
+	return b.withBuilder(b.builder.Limit(limit))
 }
 
-func (s UpdateBuilder) Do() error {
-	if s.err != nil {
-		return s.err
+// Offset sets a OFFSET clause on the query.
+func (b UpdateBuilder) Offset(offset uint64) UpdateBuilder {
+	return b.withBuilder(b.builder.Offset(offset))
+}
+
+// Suffix adds an expression to the end of the query
+func (b UpdateBuilder) Suffix(sql string, args ...interface{}) UpdateBuilder {
+	return b.withBuilder(b.builder.Suffix(sql, args...))
+}
+
+// SuffixExpr adds an expression to the end of the query
+func (b UpdateBuilder) SuffixExpr(expr sq.Sqlizer) UpdateBuilder {
+	return b.withBuilder(b.builder.SuffixExpr(expr))
+}
+
+// Debug prints the UpdateBuilder state out to the provided logger
+func (b UpdateBuilder) Debug() UpdateBuilder {
+	debug(b.ctx, b.builder)
+	return b
+}
+
+// Do executes the UpdateBuilder
+func (b UpdateBuilder) Do() error {
+	if b.err != nil {
+		return b.err
 	}
-	if !s.hasChanges {
+	if !b.hasChanges {
 		log.Println("Skipping write to DB - no updates set")
 		return nil
 	}
-	_, err := s.builder.RunWith(s.runner).ExecContext(s.ctx)
+	_, err := b.builder.RunWith(b.runner).ExecContext(b.ctx)
 	return err
 }
 
-func (s UpdateBuilder) withError(err error) UpdateBuilder {
-	if s.err != nil {
-		return s
+func (b UpdateBuilder) withError(err error) UpdateBuilder {
+	if b.err != nil {
+		return b
 	}
-	return UpdateBuilder{builder: s.builder, runner: s.runner, ctx: s.ctx, err: err, hasChanges: s.hasChanges}
+	return UpdateBuilder{builder: b.builder, runner: b.runner, ctx: b.ctx, err: err, hasChanges: b.hasChanges}
 }
 
-func (s UpdateBuilder) withBuilder(builder sq.UpdateBuilder) UpdateBuilder {
-	return UpdateBuilder{builder: builder, runner: s.runner, ctx: s.ctx, err: s.err, hasChanges: s.hasChanges}
+func (b UpdateBuilder) withBuilder(builder sq.UpdateBuilder) UpdateBuilder {
+	return UpdateBuilder{builder: builder, runner: b.runner, ctx: b.ctx, err: b.err, hasChanges: b.hasChanges}
 }
 
-func (s UpdateBuilder) withChanges() UpdateBuilder {
-	return UpdateBuilder{builder: s.builder, runner: s.runner, ctx: s.ctx, err: s.err, hasChanges: true}
+func (b UpdateBuilder) withChanges() UpdateBuilder {
+	return UpdateBuilder{builder: b.builder, runner: b.runner, ctx: b.ctx, err: b.err, hasChanges: true}
 }
