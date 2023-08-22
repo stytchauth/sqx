75d48a44c38eb9d48f22b83292bb79a432cc54b5
Maxwell Gerber <max@stytch.com>
2023-08-21 17:36:38 -0700

    feat: Add feature parity for DeleteBuilder

--- delete.go
+++ delete.go
@@ -13,23 +13,63 @@ type DeleteBuilder struct {
 	err     error
 }
 
-func (s DeleteBuilder) Where(pred interface{}, rest ...interface{}) DeleteBuilder {
-	return s.withBuilder(s.builder.Where(pred, rest...))
+// Prefix adds an expression to the beginning of the query
+func (b DeleteBuilder) Prefix(sql string, args ...interface{}) DeleteBuilder {
+	return b.withBuilder(b.builder.Prefix(sql, args...))
 }
 
-func (s DeleteBuilder) Do() error {
-	if s.err != nil {
-		return s.err
+// PrefixExpr adds an expression to the very beginning of the query
+func (b DeleteBuilder) PrefixExpr(expr sq.Sqlizer) DeleteBuilder {
+	return b.withBuilder(b.builder.PrefixExpr(expr))
+}
+
+// From sets the table to be deleted from.
+func (b DeleteBuilder) From(from string) DeleteBuilder {
+	return b.withBuilder(b.builder.From(from))
+}
+
+// Where adds WHERE expressions to the query.
+//
+// See SelectBuilder.Where for more information.
+func (b DeleteBuilder) Where(pred interface{}, rest ...interface{}) DeleteBuilder {
+	return b.withBuilder(b.builder.Where(pred, rest...))
+}
+
+// OrderBy adds ORDER BY expressions to the query.
+func (b DeleteBuilder) OrderBy(orderBys ...string) DeleteBuilder {
+	return b.withBuilder(b.builder.OrderBy(orderBys...))
+}
+
+// Limit sets a LIMIT clause on the query.
+func (b DeleteBuilder) Limit(limit uint64) DeleteBuilder {
+	return b.withBuilder(b.builder.Limit(limit))
+}
+
+// Offset sets a OFFSET clause on the query.
+func (b DeleteBuilder) Offset(offset uint64) DeleteBuilder {
+	return b.withBuilder(b.builder.Offset(offset))
+}
+
+// Suffix adds an expression to the end of the query
+func (b DeleteBuilder) Suffix(sql string, args ...interface{}) DeleteBuilder {
+	return b.withBuilder(b.builder.Suffix(sql, args...))
+}
+
+// Do executes the DeleteBuilder
+func (b DeleteBuilder) Do() error {
+	if b.err != nil {
+		return b.err
 	}
-	_, err := s.builder.RunWith(s.runner).ExecContext(s.ctx)
+	_, err := b.builder.RunWith(b.runner).ExecContext(b.ctx)
 	return err
 }
 
-func (s DeleteBuilder) Debug() DeleteBuilder {
-	debug(s.ctx, s.builder)
-	return s
+// Debug prints the DeleteBuilder state out to the provided logger
+func (b DeleteBuilder) Debug() DeleteBuilder {
+	debug(b.ctx, b.builder)
+	return b
 }
 
-func (s DeleteBuilder) withBuilder(builder sq.DeleteBuilder) DeleteBuilder {
-	return DeleteBuilder{builder: builder, runner: s.runner, ctx: s.ctx, err: s.err}
+func (b DeleteBuilder) withBuilder(builder sq.DeleteBuilder) DeleteBuilder {
+	return DeleteBuilder{builder: builder, runner: b.runner, ctx: b.ctx, err: b.err}
 }
