072dd5d8cd08dd2def390ea2c617288778915209
Logan Gore <lgore@stytch.com>
2023-08-22 15:06:49 -0700

    Don't use interactive TTY

--- utils/wait-for-mysql.sh
+++ utils/wait-for-mysql.sh
@@ -2,7 +2,7 @@
 
 echo "Waiting for MySQL to start..."
 CONTAINER=$(docker ps --filter "name=mysql" --format "{{.ID}}")
-while ! docker exec -it "${CONTAINER}" mysql --user=sqx --password=sqx -e "SELECT 1"; do
+while ! docker exec "${CONTAINER}" mysql --user=sqx --password=sqx -e "SELECT 1"; do
 	CONTAINER=$(docker ps --filter "name=mysql" --format "{{.ID}}")
 	sleep 1
 done
